a58bd59b8aad8183dc9f217d4877a833ed794749
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-09-30 20:55:54 -0600

    Add main page and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#1f2335",
+        primary: "#4fd6be",
+        secondary: "#41a6b5",
+        highlight: "#c3e88d",
+      },
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
